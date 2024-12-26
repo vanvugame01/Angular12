f27b371399881874eb4f66ff8abbe0c9308cdf48
vanvugame01 <vanvugame01@gmail.com>
2024-12-26 11:01:45 +0700

    *ngFor (phần tử là Object). Kết hợp chút HTML/Css, chút *ngIf, [ngClass]

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,21 +3,26 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
   public name = 'Vũ Nguyễn';
-  public age=15;
-  public traiCay=['Táo','Nho','Cam','Bưởi','Mãn cầu', 'Ổi'];
-  constructor() { }
+  public age = 15;
+  public traiCay = ['Táo', 'Nho', 'Cam', 'Bưởi', 'Mãn cầu', 'Ổi'];
+  public traiCay2 = [
+    { ten: 'Táo', gia: 12, haGia: true },
+    { ten: 'Nho', gia: -4.0045, haGia: false },
+    { ten: 'Cam', gia: 14, haGia: false },
+    { ten: 'Bưởi', gia: 20, haGia: true },
+  ];
+  constructor() {}
 
   ngOnInit(): void {
-    console.log('Trái cây =', this.traiCay);
+    console.log('Trái cây =', this.traiCay2);
   }
 
-  public resetName(): void{
+  public resetName(): void {
     console.log('resetName');
-    this.name="";
+    this.name = '';
   }
-
 }
