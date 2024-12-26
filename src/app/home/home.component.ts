078812c1333bf1e3f4c4b22f90ecb178e719d384
vanvugame01 <vanvugame01@gmail.com>
2024-12-26 10:06:28 +0700

    DataBinding-ngFor

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
   public name = 'Vũ Nguyễn';
   public age=15;
+  public traiCay=['Táo','Nho','Cam','Bưởi','Mãn cầu', 'Ổi'];
   constructor() { }
 
   ngOnInit(): void {
+    console.log('Trái cây =', this.traiCay);
   }
 
   public resetName(): void{
